07e774f729afaa298064dd167f7c11fb487ef9ab
Avi7620 <114797861+Avi7620@users.noreply.github.com>
2025-09-28 15:41:36 +0530

    intial updated

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -50,7 +50,7 @@ function Admin() {
   const fetchData = async () => {
     try {
       setLoading(true);
-      const response = await fetch("https://your-backend.onrender.com/api/contact");
+      const response = await fetch("https://ecocarbonbackend.onrender.com/api/contact");
       const data = await response.json();
       setFormSubmissions(data);
     } catch (error) {
